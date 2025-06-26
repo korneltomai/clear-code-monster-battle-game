312d360104af6e11586a871b80a3a7e0018fd442
T3tr1s <tomai.kornelmunka@gmail.com>
2025-06-26 14:38:51 +0200

    Add: Monster Battle setup

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 from settings import *
 from support import *
-from timer import Timer
+from custom_timer import Timer
+from monster import Monster, Opponent
+from random import choice
 
 class Game:
     def __init__(self):
@@ -10,8 +12,19 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
+        self.import_assets()
+
         # groups 
         self.all_sprites = pygame.sprite.Group()
+
+        # data
+        player_monster_list = ["Sparchu", "Cleaf", "Jacana"]
+        self.player_monsters = [Monster(self.back_surfs[name], name) for name in player_monster_list]
+        self.monster = self.player_monsters[0]
+        self.all_sprites.add(self.monster)
+
+        random_monster = choice(list(MONSTER_DATA.keys()))
+        self.opponent = Opponent(self.all_sprites, self.front_surfs[random_monster], random_monster)
 
     def run(self):
         while self.running:
@@ -24,11 +37,23 @@ class Game:
             self.all_sprites.update(dt)
 
             # draw  
+            self.display_surface.blit(self.bg_surfs["bg"], (0,0))
+            self.draw_monster_floor()
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         
         pygame.quit()
-    
+
+    def import_assets(self):
+        self.bg_surfs = folder_importer("images", "other")
+        self.back_surfs = folder_importer("images", "back")
+        self.front_surfs = folder_importer("images", "front")
+
+    def draw_monster_floor(self):
+        for sprite in self.all_sprites:
+            floor_rect = self.bg_surfs["floor"].get_frect(center = sprite.rect.midbottom + pygame.Vector2(0, -10))
+            self.display_surface.blit(self.bg_surfs["floor"], floor_rect)
+
 if __name__ == '__main__':
     game = Game()
     game.run()
